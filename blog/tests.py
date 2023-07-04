76e44847a0b10a1ace730dfca4118464964fc4ff
est091 <ellis0078@gmail.com>
2023-07-04 10:32:47 +0530

    Minor update to the structure.

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Post
 
-# Create your tests here.
+
+class ModelTesting(TestCase):
+
+    def setUp(self):
+        self.blog = Post.objects.create(title='Django', author='Ellis Sanjay Tarmaster', slug='Django')
+
+    def test_post_model(self):
+        d = self.blog
+        self.assertTrue(isinstance(d, Post))
+        self.assertEqual(str(d), 'Django')
